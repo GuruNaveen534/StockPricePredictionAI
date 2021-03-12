b83fb0b885d2cd692143e1138cdfd81e1de6980f
parksejun24 <sunnystpwns@gmail.com>
2021-03-13 02:47:59 +0900

    midnight

--- StockPricePrediction.py
+++ StockPricePrediction.py
@@ -8,8 +8,15 @@ from datetime import datetime, timedelta, date
 koreaStock = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download', header=0)[0]
 koreaStock = koreaStock.rename(columns={'회사명':'name','종목코드':'stockCode','상장일':'stockBirth'})
 koreaStock = koreaStock[['name', 'stockCode','stockBirth']]
-userSerchingStock = input()
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', -1)
+
+print(koreaStock)
+
+userSerchingStock = input()
 stockCode = koreaStock[koreaStock['name'].isin([userSerchingStock])].iloc[0,1]
 stockCode = str(stockCode).zfill(6) + '.KS'
 
@@ -25,8 +32,8 @@ yesterday = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
 start = datetime(int(stockBirth[0]), int(stockBirth[1]), int(stockBirth[2]))
 done = datetime(Nyear,Nmonth,Nday)
 
-APPLE = pdr.DataReader(stockCode, "yahoo", start, done)
-realityDF = pd.DataFrame({'ds': APPLE.index, 'y' : APPLE['Close']})
+searchStock = pdr.DataReader(stockCode, "yahoo", start, done)
+realityDF = pd.DataFrame({'ds': searchStock.index, 'y' : searchStock['Close']})
 
 model = Prophet()
 model.fit(realityDF)
@@ -35,18 +42,20 @@ future = model.make_future_dataframe(periods=365)
 forecast = model.predict(future)
 
 futureData = forecast[forecast['ds'].isin([yesterday])]
-futureData = futureData.iloc[0,18]
+futureData = int(futureData.iloc[0,18])
 
 todayData = realityDF.loc[yesterday : yesterday]
-todayData = todayData.iloc[0,1]
+todayData = int(todayData.iloc[0,1])
 
-ErrorValue = todayData - futureData
-dataSet = str(yesterday) + '  ' + str(ErrorValue) 
+errorValue = todayData - futureData
+dataSet = str(yesterday) + '  ' + str(errorValue) 
+
 print(dataSet + '\n')
-print("nowData", todayData,'원', ' | ', "futureData", futureData,'원') 
+print("nowData", todayData,'won', ' | ', 'futureData', futureData, 'won', ' | ', 'errorValue', errorValue, 'won') 
+
 model.plot(forecast) 
 #model.plot_components(forecast)
 mpl.show()
 
 
-
+#입력오류 예외처리 기능 구현하기
