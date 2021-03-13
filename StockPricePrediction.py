5fb954c9f40199b8b3c423573dcb3c4fbb048a4f
parksejun24 <sunnystpwns@gmail.com>
2021-03-13 11:32:05 +0900

    재귀함수 활용하여 사용자 입력값 받기 성공

--- StockPricePrediction.py
+++ StockPricePrediction.py
@@ -9,28 +9,24 @@ koreaStock = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=
 koreaStock = koreaStock.rename(columns={'회사명':'name','종목코드':'stockCode','상장일':'stockBirth'})
 koreaStock = koreaStock[['name', 'stockCode','stockBirth']]
 
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', -1)
-
-print(koreaStock)
-
-userSerchingStock = input()
-stockCode = koreaStock[koreaStock['name'].isin([userSerchingStock])].iloc[0,1]
-stockCode = str(stockCode).zfill(6) + '.KS'
-
-stockBirth = koreaStock[koreaStock['name'].isin([userSerchingStock])].iloc[0,2]
-stockBirth = str(stockBirth).split('-')
-print(stockBirth[0],stockBirth[1],stockBirth[2])
-
-Nyear = datetime.today().year
-Nmonth = datetime.today().month
-Nday = datetime.today().day
+today = [datetime.today().year, datetime.today().month, datetime.today().day]
 yesterday = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
 
+def searchStockCode():
+    try :
+        userSerchingStock = input()
+        stockCode = koreaStock[koreaStock['name'].isin([userSerchingStock])].iloc[0,1]
+        stockCode = str(stockCode).zfill(6) + '.KS'
+        stockBirth = koreaStock[koreaStock['name'].isin([userSerchingStock])].iloc[0,2]
+        stockBirth = str(stockBirth).split('-')
+        return stockCode,stockBirth
+    except:
+        return searchStockCode()
+    
+stockCode,stockBirth = searchStockCode()
+
 start = datetime(int(stockBirth[0]), int(stockBirth[1]), int(stockBirth[2]))
-done = datetime(Nyear,Nmonth,Nday)
+done = datetime(today[0],today[1],today[2])
 
 searchStock = pdr.DataReader(stockCode, "yahoo", start, done)
 realityDF = pd.DataFrame({'ds': searchStock.index, 'y' : searchStock['Close']})
@@ -54,8 +50,6 @@ print(dataSet + '\n')
 print("nowData", todayData,'won', ' | ', 'futureData', futureData, 'won', ' | ', 'errorValue', errorValue, 'won') 
 
 model.plot(forecast) 
-#model.plot_components(forecast)
+model.plot_components(forecast)
 mpl.show()
 
-
-#입력오류 예외처리 기능 구현하기
